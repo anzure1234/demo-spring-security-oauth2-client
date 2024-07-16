4bb4bc52fc096916a512bca60cfe40f084ae6ba4
Dung1109 <124704925+Dung1109@users.noreply.github.com>
2024-07-17 01:00:31 +0700

    first cm

---
+++ src/main/java/org/andy/demospringsecurityoauth2client/DemoSpringSecurityOauth2ClientApplication.java
@@ -0,0 +1,13 @@
+package org.andy.demospringsecurityoauth2client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringSecurityOauth2ClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringSecurityOauth2ClientApplication.class, args);
+    }
+
+}
